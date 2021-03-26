ee210f073fb2c934e1a7a62e7c0a93d5b0988f4f
Ajax23 <github@kraushh.de>
2021-03-26 09:51:48 +0100

    Remove comments

--- poresim/templates/auto_dens.py
+++ poresim/templates/auto_dens.py
@@ -13,29 +13,28 @@ if __name__ == "__main__":
     # cd ../ana
     # sh ana.sh
     # python ana.py
-    print("Uncomment following script")
 
-    # # Load molecule
-    # mol = pms.Molecule("molecule", "MOLSHORT", inp="MOLLINK")
-    #
-    # # Sample density
-    # sample = pa.Sample("../_gro/pore_system.obj", "traj.xtc", mol, is_nojump=True)
-    # sample.init_density("dens.obj")
-    # sample.sample(is_parallel=True)
-    #
-    # # Calculate density
-    # dens = pa.density.calculate("dens.obj", target_dens=TARGETDENS)
-    #
-    # # Create plot
-    # pa.density.plot(dens)
-    # plt.gcf().suptitle(r"In: $\rho=$"+"%7.3f"%dens["in"][3]+r" kg m$^{-3}$, Out: $\rho=$"+"%7.3f"%dens["ex"][3]+r" kg m$^{-3}$")
-    # plt.savefig("density.pdf", format="pdf", dpi=1000)
-    #
-    # # Fill and rerun
-    # dens_ex = dens["ex"][3]
-    # num_diff = dens["diff"]
-    # if num_diff > 10:
-    #     ps.utils.copy("../_fill/fillBackup.sh", "../_fill/fill.sh")
-    #     ps.utils.replace("../_fill/fill.sh", "FILLDENS", str(int(num_diff)))
-    #
-    #     os.system("cd ../_fill;sh fill.sh;cd ../min;SUBMITCOMMAND")
+    # Load molecule
+    mol = pms.Molecule("molecule", "MOLSHORT", inp="MOLLINK")
+
+    # Sample density
+    sample = pa.Sample("../_gro/pore_system.obj", "traj.xtc", mol, is_nojump=True)
+    sample.init_density("dens.obj")
+    sample.sample(is_parallel=True)
+
+    # Calculate density
+    dens = pa.density.calculate("dens.obj", target_dens=TARGETDENS)
+
+    # Create plot
+    pa.density.plot(dens)
+    plt.gcf().suptitle(r"In: $\rho=$"+"%7.3f"%dens["in"][3]+r" kg m$^{-3}$, Out: $\rho=$"+"%7.3f"%dens["ex"][3]+r" kg m$^{-3}$")
+    plt.savefig("density.pdf", format="pdf", dpi=1000)
+
+    # Fill and rerun
+    dens_ex = dens["ex"][3]
+    num_diff = dens["diff"]
+    if num_diff > 10:
+        ps.utils.copy("../_fill/fillBackup.sh", "../_fill/fill.sh")
+        ps.utils.replace("../_fill/fill.sh", "FILLDENS", str(int(num_diff)))
+
+        os.system("cd ../_fill;sh fill.sh;cd ../min;SUBMITCOMMAND")
